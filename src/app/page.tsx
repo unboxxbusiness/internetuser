a846e50d4f3a5b267305d85c39648a419c631479
Anuj Sharma <anujsharmatalks@gmail.com>
2025-08-31 14:24:01 +0000

    remove this section from home page Latest News & Offers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { Hero } from "@/components/landing/hero";
 import { Offers } from "@/components/landing/offers";
-import { News } from "@/components/landing/news";
 import { CTA } from "@/components/landing/cta";
 
 export default function LandingPage() {
@@ -9,7 +8,6 @@ export default function LandingPage() {
       <main className="flex-1">
         <Hero />
         <Offers />
-        <News />
         <CTA />
       </main>
     </div>
