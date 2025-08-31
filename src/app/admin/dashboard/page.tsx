1e6f933389c1999f5f55681db652b06df6e910a0
Anuj Sharma <anujsharmatalks@gmail.com>
2025-08-31 10:53:05 +0000

    on admin dashboard write admin dashboard

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -49,6 +49,9 @@ export default async function AdminDashboardPage() {
 
   return (
     <div className="flex-1 space-y-4">
+       <div className="flex items-center justify-between space-y-2">
+          <h2 className="text-3xl font-bold tracking-tight">Admin Dashboard</h2>
+        </div>
       <Tabs defaultValue="overview" className="space-y-4">
         <div className="flex items-center justify-between">
          <TabsList>
