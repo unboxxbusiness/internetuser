32783fedb4fb0813ce23e3e10b43f6fe6ae118f5
Anuj Sharma <anujsharmatalks@gmail.com>
2025-09-01 07:02:44 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <HeroSection>
    
    wit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
-import { HeroSection } from "@/components/landing/new-hero";
+import { redirect } from "next/navigation";
+import { getUser } from "./auth/actions";
 
-export default function LandingPage() {
+export default async function LandingPage() {
+  const user = await getUser();
+
+  if (user) {
+    if (user.role === "admin") {
+      redirect("/admin/dashboard");
+    } else {
+      redirect("/user/dashboard");
+    }
+  }
+  
   return (
     <div className="flex flex-col min-h-[calc(100vh-4rem)]">
       <main className="flex-1">
-        <HeroSection />
+        
       </main>
     </div>
   );
